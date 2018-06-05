d686333b391033456d7e0ed5a3163c3308c71881
ddwag1 <ddwag1@gmail.com>
2018-06-05 00:53:20 -0700

    added options todos

--- src/APIService.js
+++ src/APIService.js
@@ -12,6 +12,12 @@ const get = (route, options) => {
         baseUrl += defaultVersion
     }
     
+    //TODO: Add options for order
+    //TODO: Add options for page
+    //TODO: Add options for per_page
+    //TODO: Add hidden options for rootUrl
+    //TODO: Think of other shit to add to options    
+    
     baseUrl += route
     
     console.log('Requesting ' + baseUrl);
